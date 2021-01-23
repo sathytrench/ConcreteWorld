778ec01efb4765659cb2d73fc7c682779963c580
anahit <sathytrench@gmail.com>
2021-01-23 17:43:32 -0500

    feat: created Start Scene

---
+++ src/scenes/StartScene.js
@@ -0,0 +1,31 @@
+import 'phaser';
+
+export default class StartScene extends Phaser.Scene {
+    constructor() {
+      super('StartScene');
+      this.intro = 
+      `Hello, player! 
+
+    Another grey day in Concrete World. 
+
+    Collect the flowers! 
+      
+    Watch out for creatures!
+    
+    Press SPACE to begin...`;
+    }
+    
+    preload() {
+    }
+  
+    create() {
+      this.add.text(200, 200, this.intro);
+      this.cursors = this.input.keyboard.createCursorKeys(); 
+    }
+
+    update() {
+        if (this.cursors.space.isDown) {
+            this.scene.start('MainScene');
+        }
+    }
+  }
