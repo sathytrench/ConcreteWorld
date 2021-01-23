ec5520eddaf18dcec86d1370ef95cf42e113fa0d
anahit <sathytrench@gmail.com>
2021-01-23 18:09:12 -0500

    feat: added background music to Main Scene, plays when space bar is triggered in Start Scene

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -5,8 +5,13 @@ export default class MainScene extends Phaser.Scene {
     super('MainScene');
   }
 
+  preload() {
+    this.load.audio('music', 'assets/audio/snowWorld.mp3');
+  }
+
   create() {
     this.scene.launch('BgScene');
     this.scene.launch('FgScene');
+    this.sound.add('music').setLoop(true).play();
   }
 }
