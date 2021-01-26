623ea6bab3e95783ee3fb328274291eb4838961f
anahit <sathytrench@gmail.com>
2021-01-25 20:08:10 -0500

    added body parsing middleware and app.get'*' to server.js

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
+
+module.exports = app;
